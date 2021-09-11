c2d01afde8b48dbc68f85c13872a92d412b299bf
Fabián Rivera <frakxman@hotmail.com>
2021-09-10 22:45:31 -0500

    Add Not Foud page

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Styles 
+import '../assets/styles/components/NotFound.scss';
+import logo from '../assets/statics/react.png';
+
+const NotFound = () => (
+    <section className="error">
+        <section className="error__container">
+            <h2 className="error__container--title">
+                4 <sub><img src={ logo } alt="" /></sub>4
+            </h2>
+            <p>Page Not Found</p>
+        </section>
+    </section>
+);
+
+export default NotFound;
