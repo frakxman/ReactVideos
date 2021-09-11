4e3d6b283ddbf29db639be99122005201ebb704c
Fabián Rivera <frakxman@hotmail.com>
2021-09-10 21:20:16 -0500

    Add routes

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -16,7 +16,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
 
     const initialState = useInitialState( API );
 
@@ -55,4 +55,4 @@ const App = () => {
     )
 };
 
-export default App;
+export default Home;
