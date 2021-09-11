0e760cf2965733fb74bc4e3f39ed82131feede93
Fabián Rivera <frakxman@hotmail.com>
2021-09-11 02:05:29 -0500

    Add form managment and gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+// Utils 
+import gravatar from '../utils/gravatar';
 
 // Styles 
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/statics/react.gif';
 import userIcon from '../assets/statics/user.png';
 
-const Header = () => (
-    <header className="header">
-        <Link to="/">
-            <img className="header__img" src={ logo } alt="React Videos" />
-        </Link>
-        <div className="header__menu">
-            <div className="header__menu--profile">
-                <img src={ userIcon } alt="user icon" />
-                <p>Profile</p>
+const Header = props => {
+    const { user } = props;
+    return (
+        <header className="header">
+            <Link to="/">
+                <img className="header__img" src={ logo } alt="React Videos" />
+            </Link>
+            <div className="header__menu">
+                <div className="header__menu--profile">
+                    <img src={ Object.keys( user ).length > 0 ? gravatar( user.email ) : userIcon } alt={ user.email } />
+                    <p>Profile</p>
+                </div>
+                <ul>
+                    <li><a href="/">Acount</a></li>
+                    <li><Link to="/login">Logout</Link></li>
+                </ul>
             </div>
-            <ul>
-                <li><a href="/">Acount</a></li>
-                <li><Link to="/login">Logout</Link></li>
-            </ul>
-        </div>
-    </header>
-);
+        </header>
+    )
+};
 
-export default Header;
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    };
+};
+
+export default connect( mapStateToProps, null ) ( Header );
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,45 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+// Actions 
+import { loginRequest } from '../actions';
 
 // Styles
 import '../assets/styles/components/Login.scss';
 import google from '../assets/statics/google.jpg';
 import twitter from '../assets/statics/twitter.png';
 
-const Login = () => (
-    <section className='login'>
-		<section className='login__container'>
-			<h2>Login</h2>
-			<form className='login__container--form'>
-				<input className='input' type='email' placeholder='Email' />
-				<input className='input' type='password' placeholder='Password' />
-				<button className='button'>
-                    Sign In
-                </button>
-				<div className='login__container--remember-me'>
-					<label>
-						<input type='checkbox' id='cbox1' value='first_checkbox' />
-						Remember me
-					</label>
-					<a href='/'>Forgot my password</a>
-				</div>
-			</form>
-			<section className='login__container--social-media'>
-				<div>
-					<img src={ google } /> Sign in with Google
-				</div>
-				<div>
-					<img src={ twitter } /> Sign in with Twitter
-				</div>
-			</section>
-			<p className='login__container--register'>
-				You don't have any account 
-                <Link to='/register'>
-                    Register
-                </Link>
-			</p>
-		</section>
-	</section>
-);
+const Login = props => {
+	const [ form, setValues ] = useState({
+		email: '',
+	});
 
-export default Login;
+	const handleInput = event => {
+		setValues({
+			...form,
+			[ event.target.name ]: event.target.value
+		});
+	};
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		props.loginRequest( form );
+		props.history.push('/');
+	};
+
+	return (
+		<section className='login'>
+			<section className='login__container'>
+				<h2>Login</h2>
+				<form className='login__container--form' onSubmit={ handleSubmit }>
+					<input
+						name='email'
+						className='input'
+						type='email'
+						placeholder='Email'
+						onChange={ handleInput }
+					/>
+					<input
+						name='password'
+						className='input'
+						type='password'
+						placeholder='Password'
+						onChange={ handleInput }
+					/>
+					<button className='button'>
+						Sign In
+					</button>
+					<div className='login__container--remember-me'>
+						<label>
+							<input type='checkbox' id='cbox1' value='first_checkbox' />
+							Remember me
+						</label>
+						<a href='/'>Forgot my password</a>
+					</div>
+				</form>
+				<section className='login__container--social-media'>
+					<div>
+						<img src={ google } /> Sign in with Google
+					</div>
+					<div>
+						<img src={ twitter } /> Sign in with Twitter
+					</div>
+				</section>
+				<p className='login__container--register'>
+					You don't have any account 
+					<Link to='/register'>
+						Register
+					</Link>
+				</p>
+			</section>
+		</section>
+	)
+};
+
+const mapDispatchToProps = {
+	loginRequest
+}
+
+export default connect( null, mapDispatchToProps ) ( Login );
