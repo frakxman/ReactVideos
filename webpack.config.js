305a40a32b4578bb0c36ea9dd6a2d0952bec8215
Fabián Rivera <frakxman@hotmail.com>
2021-09-16 10:19:06 -0500

    Minimize bundle by Terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 require('dotenv').config();
 
@@ -23,6 +24,10 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+    optimization: {
+        minimize: true,
+        minimizer:  [ new TerserPlugin() ],
+    },
     module: {
         rules: [
             {
@@ -31,14 +36,6 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 },
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    },
-                ],
             },
             {
                 test: /\.(s*)css$/,
@@ -59,6 +56,10 @@ module.exports = {
     },
     plugins: [
         isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
+        isDev ? () => {} : new CompressionWebpackPlugin({
+            test: /\.js$|\.css$/,
+            filename: '[path][base].gz',
+        }),
         new MiniCssExtractPlugin({
             filename: 'assets/app.css',
         }),
