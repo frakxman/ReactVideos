499c50fcd476abdc585c29c5cb1d01d2714792d1
Fabián Rivera <frakxman@hotmail.com>
2021-09-17 09:14:36 -0500

    Add manifest

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,7 @@ import { createStore } from 'redux';
 import { renderRoutes } from 'react-router-config';
 import { StaticRouter } from 'react-router-dom';
 import serverRoutes from '../frontend/routes/serverRoutes';
+import getManifest from './getManifest';
 
 import initialState from '../frontend/initialState';
 
@@ -17,7 +18,6 @@ import reducer from '../frontend/reducers';
 
 dotenv.config();
 const { ENV, PORT } = process.env;
-
 const app = express();
 
 if ( ENV === 'development' ) {
@@ -31,6 +31,10 @@ if ( ENV === 'development' ) {
     app.use( webpackDevMiddleware( compiler, serverConfig ) );
     app.use( webpackHotMiddleware( compiler ) );
 } else {
+    app.use(( req, res, next ) => {
+        if ( !req.hashManifest ) req.hashManifest = getManifest();
+        next();
+    });
     app.use( express.static(`${__dirname}/public`));
     app.use( helmet());
     app.use( helmet.permittedCrossDomainPolicies() );
@@ -52,12 +56,14 @@ if ( ENV === 'development' ) {
     app.disable('x-powered-by');
 }
 
-const setResponse = ( html, preloadedState ) => {
+const setResponse = ( html, preloadedState, manifest ) => {
+    const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+    const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
     return (`
         <!DOCTYPE html>
             <html>
                 <head>
-                <link rel="stylesheet" href="assets/app.css" type="text/css">
+                <link rel="stylesheet" href="${ mainStyles }" type="text/css">
                     <title>React Videos</title>
                 </head>
                 <body>
@@ -65,7 +71,7 @@ const setResponse = ( html, preloadedState ) => {
                     <script>
                         window.__PRELOADED_STATE__ = ${ JSON.stringify( preloadedState ).replace(/</g, '\\u003c') }
                     </script>
-                    <script src="assets/app.js" type="text/javascript"></script>
+                    <script src="${ mainBuild }" type="text/javascript"></script>
                 </body>
             </html>
     `);
@@ -85,7 +91,7 @@ const renderApp = ( req, res ) => {
         </Provider>,
     );
 
-    res.send( setResponse( html, preloadedState ) )
+    res.send( setResponse( html, preloadedState, req.hashManifest ) )
 };
 
 app.get('*', renderApp);
